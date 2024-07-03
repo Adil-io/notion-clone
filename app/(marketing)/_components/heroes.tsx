4aad9274f10bc71889392e70cc4608c2b47ba756
Adil Yusuf <adilyusuf786@gmail.com>
2024-07-03 12:50:09 +0530

    Dark mode implemented

---
+++ app/(marketing)/_components/heroes.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+
+export const Heroes = () => {
+  return (
+    <div className="flex flex-col items-center justify-center max-w-5xl">
+      <div className="flex items-center">
+        <div className="relative size-[300px] sm:size-[350px] md:size-[400px]">
+          <Image
+            src="/documents.png"
+            fill
+            className="object-contain dark:hidden"
+            alt="Documents"
+          />
+          <Image
+            src="/documents-dark.png"
+            fill
+            className="object-contain hidden dark:block"
+            alt="Documents"
+          />
+        </div>
+        <div className="relative size-[400px] hidden md:block">
+          <Image
+            src="/reading.png"
+            fill
+            className="object-contain dark:hidden"
+            alt="Reading"
+          />
+          <Image
+            src="/reading-dark.png"
+            fill
+            className="object-contain hidden dark:block"
+            alt="Reading"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
